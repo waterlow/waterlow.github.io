fc6c80bbe2d9ef58e0c3c97e3d7ef5ace1f595b1
Ohta Akira <waterlow3442@gmail.com>
2021-03-02 00:34:22 +0900

    wip

--- animated_login_form/js/index.js
+++ animated_login_form/js/index.js
@@ -1,3 +1,11 @@
 (() => {
-  console.log('hello')
+  document.querySelector('.text-input input').addEventListener('focus', (event) => {
+    event.target.classList.add('focus')
+  })
+
+  document.querySelector('.text-input input').addEventListener('blur', (event) => {
+    if(event.target.value === '') {
+      event.target.classList.remove('focus')
+    }
+  })
 })()
